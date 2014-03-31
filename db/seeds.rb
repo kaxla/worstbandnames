9ba6654850f646717c51d1d03b714c3b54746a8c
kaxla <kaylas.email.address@gmail.com>
2014-03-30 20:43:36 -0700

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+@band1 = Bandname.create(name: "Oxygen Ponies")
+@band2 = Bandname.create(name: "Blue Floyd")
+@band3 = Bandname.create(name: "Pudding Attack")
+@band4 = Bandname.create(name: "Goth Brooks")
+@band5 = Bandname.create(name: "Majical Cloudz")
+@band6 = Bandname.create(name: "Dungeonesse")
+@band7 = Bandname.create(name: "The Front Bottoms")
+@band8 = Bandname.create(name: "Laminated Cat")
+@band9 = Bandname.create(name: "Stardeath and the White Dwarfs")
+@band10 = Bandname.create(name: "The Pigeon Detectives")
