f6983c12fcb5975caf07fa9b793286ec51d62d8a
kaxla <kaylas.email.address@gmail.com>
2014-03-30 19:50:54 -0700

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Worstbandnames::Application.routes.draw do
+  devise_for :users
   resources :bandnames
   # get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
